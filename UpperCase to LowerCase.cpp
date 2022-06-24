01e383fda011ec8643a67c45f8d0a5fa3e834b31
Madiha Gurchani <32651505+MadihaGurchani@users.noreply.github.com>
2022-06-24 15:11:49 +0500

    Add files via upload

---
+++ UpperCase to LowerCase.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+# include <ctype.h>
+# include <stdlib.h>
+void convertToLowercase (char *);
+int main ()
+{
+ char s[30] = "WELCOME TO VIRTUAL UNIVERSITY";
+ cout << "The string before conversion is: " << s << endl ;
+convertToLowercase ( s) ; //function call
+ cout << "“The string after conversion is: " << s ;
+}
+void convertToLowercase (char *sptr)
+{
+ while ( *sptr != NULL )
+ {
+     if ( isupper ( *sptr) )
+        *sptr = tolower ( *sptr ); 
+    ++ sptr; 
+ } 
+}
